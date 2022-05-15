9b794043e568b2d9edf309981a40317124e6dab7
dashakozachenko <dkozacenko4@gmail.com>
2022-05-15 14:25:55 +0200

    Corrected

--- js/script.js
+++ js/script.js
@@ -5,19 +5,17 @@ const calculator = {
     read() {
         this.firstNum = +prompt('Input first num:', '2');
         this.secondNum = +prompt('Input second num:', '3');
+
+        if(isNaN(this.firstNum) || isNaN(this.secondNum)) {
+            return console.log( 'Invalid value')
+        }
     },
 
     sum() {
-        if (isNaN(this.firstNum) || isNaN(this.secondNum)) {
-           return console.log("Invalid value");
-        }
         return this.firstNum + this.secondNum;
     },
 
     mul() {
-        if (isNaN(this.firstNum) || isNaN(this.secondNum)) {
-            return console.log("Invalid value");
-        }
         return this.firstNum * this.secondNum;
     }
 }
